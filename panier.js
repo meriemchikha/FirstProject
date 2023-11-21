9f6447aa4746f606eae0274bc92eba6ae8dfd9d1
Meriem Chikha <meriem.chikha@laposte.net>
2023-11-21 21:35:51 +0100

    myproject

---
+++ panier.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const checkoutButton = document.getElementById('checkout');
+    checkoutButton.addEventListener('click', function() {
+        // Ici, vous pouvez ajouter la logique pour passer la commande
+        alert('Commande passée !');
+    });
+});
